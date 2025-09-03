f749757c9c40271286a23c4d308d7465c3716ef8
triastartya <triastartya@gmail.com>
2025-09-03 10:08:57 +0700

    oke
    # Conflicts:
    #	diskusi tambak udang di basecamp 22 agt 2025 (1).txt

---
+++ resources/views/feature/auth/menus/script.php
@@ -0,0 +1,28 @@
+<script>
+app.controller("myCtrl", function($scope,$http) {
+    angular.element(document).ready(function () {
+        autosize($("#alamat"));
+        setTimeout(function(){
+        $("#viewtabel").DataTable({
+                scrollY: "50vh",
+                scrollX: !0,
+                scrollCollapse: !0,
+                columnDefs: [{
+                    targets: -1,
+                    title: "Actions",
+                    orderable: !1,
+                    render: function(a, e, t, n) {
+                        return `<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-warning m-btn--icon m-btn--icon-only m-btn--pill" title="View"><i class="la la-edit"></i></a>
+                        <a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon-only m-btn--pill" title="View"><i class="la la-remove"></i></a>`
+                    }
+                }],
+            })
+        }, 500);
+    });
+    $scope.tes = "tes";
+    $scope.tambah = function(){
+        console.log("tambah lokasi");
+        $('#m_create').modal('show');
+    }
+});
+</script>
