38ffb10bd62d7cedc045260e76c6850c8312bfe9
triastartya <triastartya@gmail.com>
2025-09-10 09:31:36 +0700

    migration

--- database/migrations/2025_09_08_151653_create_user_table.php
+++ database/migrations/2025_09_08_151653_create_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ms_user', function (Blueprint $table) {
+        Schema::create('setup_user', function (Blueprint $table) {
             $table->id('id_user');
             $table->string('nama',100);
             $table->string('username',100);
