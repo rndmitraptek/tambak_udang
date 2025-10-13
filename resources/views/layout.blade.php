0df73549ae9a9b26843e588a23ca83b2c163143c
triastartya <triastartya@gmail.com>
2025-10-13 10:58:40 +0700

    dashbaord

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -399,7 +399,7 @@ License: You must have a valid license purchased only from themeforest(the above
 					<div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-light m-aside-menu--submenu-skin-light " m-menu-vertical="1" m-menu-scrollable="1" m-menu-dropdown-timeout="500" style="position: relative;">
 						<ul class="m-menu__nav  m-menu__nav--dropdown-submenu-arrow ">
 							<li class="m-menu__item  m-menu__item--active" aria-haspopup="true">
-								<a href="dashboard" class="m-menu__link ">
+								<a href="/dashboard" class="m-menu__link ">
 									<i class="m-menu__link-icon flaticon-line-graph"></i>
 									<span class="m-menu__link-title">
 										<span class="m-menu__link-wrap">
