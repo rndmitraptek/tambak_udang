4b12626dd8d941563e88475e625c841a724b976c
triastartya <triastartya@gmail.com>
2025-11-05 16:14:36 +0700

    tunai

--- app/Models/Finance/PembayaranPiutangCustomerTunaiModel.php
+++ app/Models/Finance/PembayaranPiutangCustomerTunaiModel.php
@@ -9,5 +9,5 @@ class PembayaranPiutangCustomerTunaiModel extends Model
     //
     protected $table = 'pembayaran_piutang_customer_tunai';
     protected $primaryKey = 'id_pembayaran_piutang_customer';
-    protected $fillable = ['id_pembayaran_piutang_customer','tanggal_bayar','nama_penerima','nama_pemberi','nominal'];
+    protected $fillable = ['id_pembayaran_piutang_customer','tanggal_bayar','id_coa','kode_coa','nama_penerima','nama_pemberi','nominal'];
 }
